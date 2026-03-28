4e501c5d18008dd8f8789290dc2f2d46d3a91ae0
real-kicktheball <chanhuorgo0503@gmail.com>
2026-03-28 17:07:27 +0900

    Create 2920.py

---
+++ 2920.py
@@ -0,0 +1,8 @@
+a, b, c, d, e, g, h, i = map(int, input().split())
+
+if a == 1 and b == 2 and c == 3 and d == 4 and e == 5 and g == 6 and h == 7 and i == 8:
+    print("ascending")
+elif a == 8 and b == 7 and c == 6 and d == 5 and e == 4 and g == 3 and h == 2 and i == 1:
+    print("descending")
+else:
+    print("mixed")
